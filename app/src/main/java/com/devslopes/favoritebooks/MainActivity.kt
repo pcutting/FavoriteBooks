c87b1ba02bc20afe917eb579b56ec526e74ffd03
Phil Cutting <phil@nevie.com>
2021-02-09 16:04:07 +0200

    hide fab on scroll

--- app/src/main/java/com/devslopes/favoritebooks/MainActivity.kt
+++ app/src/main/java/com/devslopes/favoritebooks/MainActivity.kt
@@ -28,6 +28,33 @@ class MainActivity : AppCompatActivity() {
             layoutManager = LinearLayoutManager(this@MainActivity, RecyclerView.VERTICAL, false)
             adapter = bAdapter
             //addItemDecoration(SimpleDividerItemDecoration(this))
+//            addOnScrollListener( OnScrollListener{
+//              override fun onScrolled(recycerView: RecyclerView, dx : Int, dy : Int){
+//                  if (dy > 0 || dy < 0 && binding.fab.isVisible) {
+//                      binding.fab.hide()
+//                  }
+//              }
+//            })
+
+            binding.bookList.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+                override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                    if (dy > 0 || dy < 0 && binding.fab.isShown) {
+                        binding.fab.hide()
+                    }
+                }
+
+                override fun onScrollStateChanged(
+                    recyclerView: RecyclerView,
+                    newState: Int
+                ) {
+                    if (newState == RecyclerView.SCROLL_STATE_IDLE) {
+                        binding.fab.show()
+                    }
+                    super.onScrollStateChanged(recyclerView, newState)
+                }
+            })
+                //----------
+
 
         }
 
